50086406b533171952527adb49edf29f45505f6a
jainvishal520 <vjtinu.jain431@gmail.com>
2017-08-07 01:26:44 +0530

    directives

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -2,14 +2,25 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-server',
-  templateUrl: './server.component.html'
+  templateUrl: './server.component.html',
+  styles: [`
+    .online{
+      color:#fff;
+    }
+  `]
 })
 
 export class ServerComponent{
   id: number = 10;
   status: String = 'online';
+  constructor() {
+    this.status = Math.random() > 0.5 ? 'online' : 'offline';
+  }
 
   get_status() {
     return this.status;
+  }
+  get_color() {
+    return this.status === 'online' ? 'green' : 'red';
   }
 }
--- src/app/servers/servers.component.ts
+++ src/app/servers/servers.component.ts
@@ -5,13 +5,22 @@ import { Component, OnInit } from '@angular/core';
   // selector: '.app-servers',// use as a class
   // selector: '[app-servers]', // use as attribute in html
   templateUrl: './servers.component.html',
-  styleUrls: ['./servers.component.css']
+  // styleUrls: ['./servers.component.css']
+  styles: [`
+    .white{
+      color:#fff;
+    }
+  `]
 })
 export class ServersComponent implements OnInit {
   addMoreServers = false;
   serverCreationStatus = 'No server was created!!!';
   serverName = 'test name';
-
+  serverStatus = false;
+  servers= ['testserver1', 'testserver2'];
+  displaySecret = false;
+  clickStats = [];
+  i = 0;
   // username: '';
 
   constructor() {
@@ -23,11 +32,22 @@ export class ServersComponent implements OnInit {
   ngOnInit() {
   }
   onCreateServer() {
+    this.serverStatus = true;
+    this.servers.push(this.serverName);
     this.serverCreationStatus = `Server was created.Server name is ${this.serverName}`;
   }
   onUpdateServerName(event: Event) {
     // console.log(event);
     this.serverName = (<HTMLInputElement>event.target).value;
+  }
+  onDisplaySecret(){
+    this.i = this.i + 1;
+    this.displaySecret = !this.displaySecret;
+    // this.clickStats.push(this.i);
+    this.clickStats.push(new Date());
+  }
+  get_blue_color(){
+    return this.i > 5 ? 'blue' : 'transparent';
   }
   // onResetUsername() {
   //   this.username = '';
